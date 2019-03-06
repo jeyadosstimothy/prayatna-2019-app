7805e2c1d327f54056257356423739958acaa9a2
Timothy <jeyadosstimothy@gmail.com>
2019-03-06 10:02:43 +0530

    Changed json reponse parsing
    Removed cupertino icons dependancy

--- lib/request.dart
+++ lib/request.dart
@@ -36,17 +36,25 @@ class HttpRequest {
       print('Request Body: ' + body);
       print('Response: ' + response.body.toString());
       if (response.statusCode == 200 || response.statusCode == 201) {
-        List responseList = (json.decode(response.body) as List);
-        int selected = responseList.removeLast()['selected'];
-        List<Team> teams = responseList.map((team) {
-          return Team(
-            rank: team['rank'].toString(),
-            members: team['names'],
-            marks: team['marks'].toString(),
-            isSelected: (team['rank'] < selected) ? true : false,
-          );
-        }).toList();
-        this.view.onLoadResultsComplete(teams, selected);
+        var decodedResponse = json.decode(response.body);
+        List responseList = (decodedResponse as List);
+        Map<String, dynamic> params = responseList.removeLast();
+        String resultsAvailable = params['published'];
+        if (resultsAvailable == 'true') {
+          int selected = params['selected'];
+          List<Team> teams = responseList.map((team) {
+            return Team(
+              rank: team['rank'].toString(),
+              members: List<String>.from(team['names']),
+              marks: team['marks'].toString(),
+              isSelected: (team['rank'] < selected) ? true : false,
+            );
+          }).toList();
+          this.view.onLoadResultsComplete(teams, selected);
+        } else {
+          this.view.onLoadResultsError(
+              'Results are not published yet. Please try again later');
+        }
       } else {
         this.view.onLoadResultsError(response.body.toString());
       }
