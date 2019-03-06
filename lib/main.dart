9eac37df637c2fb0cfdbf443e6ccdc289062ea6f
Timothy <jeyadosstimothy@gmail.com>
2019-03-06 16:33:50 +0530

    Forced Portrait Orientation

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'eventCard.dart';
 import 'eventDetails.dart';
 
@@ -7,6 +8,10 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+      DeviceOrientation.portraitDown,
+    ]);
     return MaterialApp(
       title: 'Prayatna',
       home: HomePage(title: "Prayatna '19"),
